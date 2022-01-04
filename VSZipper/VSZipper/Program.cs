2ab7f5680d1592126519e2569854aa2a8283fc43
Jaffar Ali <jaffarali@merchantsfleet.com>
2022-01-03 19:41:52 -0500

    Initial code commit

---
+++ VSZipper/VSZipper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSZipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
